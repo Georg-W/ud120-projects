380c832a3b3c2426ce21a87495915cbf9dfb143e
Georg Westner <Georg Westner>
2017-05-21 10:19:25 +0200

    svm modifications finished

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -22,19 +22,35 @@ features_train, features_test, labels_train, labels_test = preprocess()
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 
-features_train = features_train[:len(features_train)/10]
-labels_train = labels_train[:len(labels_train)/10]
-
-clf = SVC()
+features_train = features_train[:len(features_train)/1]
+labels_train = labels_train[:len(labels_train)/1]
+clf = SVC(C=10000.0, cache_size=200, class_weight=None, coef0=0.0,
+          decision_function_shape=None, degree=3, gamma='auto', kernel='rbf',
+          max_iter=-1, probability=False, random_state=None, shrinking=True,
+          tol=0.001, verbose=False)
 t0 = time()
 clf.fit(features_train, labels_train)
 print "training time:", round(time()-t0, 3), "s"
-SVC(C=1.0, kernel='linear', degree=3, gamma='auto', coef0=0.0, shrinking=True, probability=False, tol=0.001, cache_size=200, class_weight=None, verbose=False, max_iter=-1, decision_function_shape=None, random_state=None)
+
 t1 = time()
 pred = clf.predict(features_test)
-print "training time:", round(time()-t1, 3), "s"
+print "predict time:", round(time()-t1, 3), "s"
 
 print accuracy_score(pred, labels_test)
+
+counter = 0
+
+for val in pred:
+    if val == 1:
+        counter += 1
+
+print "10 ",pred[10]
+
+print "26 ",pred[26]
+
+print "50 ",pred[50]
+
+print "counter", counter
 
 
 
