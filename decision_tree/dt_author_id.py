69e48a596cdcff5831502fa560a2688d938c2959
Georg Westner <Georg Westner>
2017-05-21 11:59:09 +0200

    completed decision trees

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -19,7 +19,24 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
+features_train = features_train[:len(features_train)/1]
+labels_train = labels_train[:len(labels_train)/1]
+
+print "length", len(features_train[0])
+clf = tree.DecisionTreeClassifier(criterion='gini', splitter='best', max_depth=None, min_samples_split=40, min_samples_leaf=1, min_weight_fraction_leaf=0.0, max_features=None, random_state=None, max_leaf_nodes=None, min_impurity_split=1e-07, class_weight=None, presort=False)
+
+t1 = time()
+clf = clf.fit(features_train, labels_train)
+print "predict time:", round(time()-t1, 3), "s"
+
+t1 = time()
+pred = clf.predict(features_test)
+print "predict time:", round(time()-t1, 3), "s"
+
+print accuracy_score(pred, labels_test)
 
 
 #########################################################
