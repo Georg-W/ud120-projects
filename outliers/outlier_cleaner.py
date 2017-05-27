0119a1cfecb277d4a94dc9483976c4a13b876af5
Georg Westner <Georg Westner>
2017-05-27 14:25:36 +0200

    linear regressions completed

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -10,11 +10,42 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
+
+    print "length pred: ", len(predictions)
+    print "length ages: ", len(ages)
+    print "length net: ", len(net_worths)
+    print "pred: ", predictions[1]
+    print "new: ", net_worths[1]
+    print "diff: ", predictions[1] - net_worths[1]
+
+    errors = []
+    newArr = []
+    counter = 0
+
+    print "test: ", predictions[0][0]
+
+
+    for pred in predictions:
+        errors.append(abs(predictions[counter][0] - net_worths[counter][0]))
+        newArr.append((ages[counter][0], net_worths[counter][0], abs(predictions[counter][0] - net_worths[counter][0])))
+        counter += 1
+
+    print newArr[57]
+    newArr.sort(key=lambda x:x[2])
+
+    for error in newArr:
+        print "error: ", error[2]
+
+    print "biggest: ", newArr[0:-8]
+
+    arr = newArr[0:-10]
+
+
     cleaned_data = []
 
     ### your code goes here
+    cleaned_data = arr
 
-    
+
     return cleaned_data
 
